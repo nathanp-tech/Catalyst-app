cfbd8d4a5fa771b540706ed61e4ab1fb57f50912
nathanp-tech <nathan.paillou@gmail.com>
2025-12-01 21:24:29 +0100

    updates

--- core/settings.py
+++ core/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-#STATICFILES_DIRS = [
-#    BASE_DIR / "static",
-#]
+# Tell Django where to find your static files in development
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Directory where `collectstatic` will place all static files for production
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
