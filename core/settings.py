ef6b9cefa3b8d0896c657244af2ef421df6b9dad
nathanp-tech <nathan.paillou@gmail.com>
2026-02-17 17:03:07 +0100

    prepare the push to python anywhere

--- core/settings.py
+++ core/settings.py
@@ -95,14 +95,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    # Production configuration (Render)
-    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
-else:
-    # Local development configuration (SQLite)
-    DATABASES = {
-        'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
